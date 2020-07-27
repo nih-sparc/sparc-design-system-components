519e9b59d65bc3c2eb9253dd350575c607583763
Cameron Baney <cbaney@gmail.com>
2020-07-27 14:40:50 -0400

    Add docs addon

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
-    '@storybook/addon-links'
+    '@storybook/addon-links',
+    '@storybook/addon-docs'
   ]
 }
--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,15 @@
-import { configure } from '@storybook/vue';
+import { configure, addParameters } from '@storybook/vue';
 
 import Vue from 'vue';
 
 import SparcComponentLibrary from '../../src/components/index.js'
 
 Vue.use(SparcComponentLibrary)
+
+addParameters({
+  docs: {
+    inlineStories: true,
+  },
+})
 
 configure(require.context('../../src', true, /\.stories\.js$/), module);
