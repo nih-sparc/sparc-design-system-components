5067ed59217bd7ea2ca51527a4063ebf3b711dc6
Cameron Baney <cbaney@gmail.com>
2020-07-28 09:55:24 -0400

    MDX docs for buttons

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,15 +1,9 @@
-import { configure, addParameters } from '@storybook/vue';
+import { configure } from '@storybook/vue';
 
 import Vue from 'vue';
 
 import SparcComponentLibrary from '../../src/components/index.js'
 
 Vue.use(SparcComponentLibrary)
-
-addParameters({
-  docs: {
-    inlineStories: true,
-  },
-})
 
 configure(require.context('../../src', true, /\.stories\.js$/), module);
--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -2,31 +2,56 @@ import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 const defaultProps = () => ({
   isDisabled: {
-    default: boolean('Disabled', false)
+    default: boolean('Disabled', true)
   },
   text: {
-    default: text('Text', 'Button')
+    default: text('Text', 'Submit')
   }
 })
 
 export default {
   title: 'Button',
-  decorators: [withKnobs]
+  decorators: [withKnobs],
+  includeStories: []
 }
 
 export const Primary = () => ({
   props: defaultProps(),
-  template: `<el-button :disabled="isDisabled">{{ text }}</el-button>`
+  template: `
+    <div>
+      <el-button>{{ text }}</el-button>
+      <el-button :disabled="isDisabled">Disabled Button</el-button>
+      <el-button class="large">Large Button</el-button>
+      <el-button>Normal Button</el-button>
+      <el-button class="small">Small Button</el-button>
+    </div>
+  `
 })
+
 export const Secondary = () => ({
   props: defaultProps(),
-  template: `<el-button class="secondary" :disabled="isDisabled">{{ text }}</el-button>`
+  template: `
+    <div>
+      <el-button class="secondary">{{ text }}</el-button>
+      <el-button class="secondary" :disabled="isDisabled">Disabled Button</el-button>
+    </div>
+  `
 })
 export const SecondaryAlt = () => ({
   props: defaultProps(),
-  template: `<el-button class="secondary alt" :disabled="isDisabled">{{ text }}</el-button>`
+  template: `
+    <div>
+      <el-button class="secondary alt">{{ text }}</el-button>
+      <el-button class="secondary alt" :disabled="isDisabled">Disabled Button</el-button>
+    </div>
+  `
 })
 export const Danger = () => ({
   props: defaultProps(),
-  template: `<el-button class="danger" :disabled="isDisabled">{{ text }}</el-button>`
+  template: `
+    <div>
+      <el-button class="danger">{{ text }}</el-button>
+      <el-button class="danger" :disabled="isDisabled">Disabled Button</el-button>
+    </div>
+  `
 })
