b030e7bae8705c7daaf9115165b0c7978a2d83c9
alan-wu <alan.wu@auckland.ac.nz>
2021-04-22 22:29:02 +1200

    Add unit test for header.

--- src/components/Header/src/Header.spec.js
+++ src/components/Header/src/Header.spec.js
@@ -1,17 +1,30 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue  } from '@vue/test-utils'
 import Header from './Header'
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
 
 describe('Header', () => {
 
   const wrapper = mount(Header, {
+    localVue,
+    router,
+    stubs: ['el-input', 'el-option', 'el-select', 'svgicon'],
     propsData: {
       linkComponent: "router-link"
     }
   })
 
-  it('', async () => {
+  it('check-header', async () => {
     const header__nav = wrapper.find('.header__nav--main')
     expect(header__nav.exists()).toBe(true)
+  })
+
+  it('check-links', async () => {
+    const link = wrapper.find('.nav5')
+    expect(link.exists()).toBe(true)
   })
 
 })
