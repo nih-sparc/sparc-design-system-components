6266f97a79ac41848411444599a0cf43905557e7
alan-wu <alan.wu@auckland.ac.nz>
2021-04-22 16:17:02 +1200

    Get header to work with app using yarn serve command.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 import SparcComponentLibrary from './components/index.js'
@@ -6,7 +7,12 @@ import SparcComponentLibrary from './components/index.js'
 Vue.config.productionTip = false
 
 Vue.use(SparcComponentLibrary)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
